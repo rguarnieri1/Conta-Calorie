aa8d1d77e2a815510374a6228ec61f06c4fb370b
Roberto Guarnieri <roberto.guarnieri2@gmail.com>
2026-07-11 15:54:38 +0200

    Completamento eliminazione voce di menù weather

--- Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
+++ Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c11393504c3ad448c3fd2485c76b45ea79581f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5874d8186b34c6407ef75bdc53e5c0532971ee75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
