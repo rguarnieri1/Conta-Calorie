3b4576e75a1e87f4616238a4224d916e59ef825a
Roberto Guarnieri <roberto.guarnieri2@gmail.com>
2026-07-11 18:33:48 +0200

    Rimuove Counter.razor e riferimenti al menu

--- Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
+++ Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8faf23d510fc7b6bd70470e67e9d6c6a9f51b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da730eed576700bbb9f58e021acc5572c53f8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
