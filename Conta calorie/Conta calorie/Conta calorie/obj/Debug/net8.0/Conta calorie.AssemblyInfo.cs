f7f69c0b05fde83699dd50b2655845969fad5b14
Roberto Guarnieri <roberto.guarnieri2@gmail.com>
2026-07-11 16:10:48 +0200

    Cambio branch

--- Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
+++ Conta calorie/Conta calorie/Conta calorie/obj/Debug/net8.0/Conta calorie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168ff4f7ef435fcf585cb1c075a96853762c0a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a910dc0bd20672175c1826efb9db9be4091af648")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conta_calorie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
