a9e645df0804f8eafc33abfa099e82e2e19392d1
Roberto Guarnieri <roberto.guarnieri2@gmail.com>
2026-07-11 18:20:42 +0200

    Merge cc-02

--- Conta calorie/BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ Conta calorie/BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c11393504c3ad448c3fd2485c76b45ea79581f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1db35790a57475da311ed1a9a74001f3faeba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
