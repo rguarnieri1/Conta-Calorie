1d9dfd4f91d5f0183ae0921c511d586bf801a34b
Roberto Guarnieri <roberto.guarnieri2@gmail.com>
2026-07-19 22:58:14 +0200

    Merge cc-09

--- Conta calorie/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ Conta calorie/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee7025d1ea4510a1604e6c11f3f82fa98c48822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d834e2ab5932649500eda309cea3f1e2c562e152")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
